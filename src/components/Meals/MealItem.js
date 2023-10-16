28ec8196ca8a0b8d878b6cf6cda1e9ed0cd43d1c
AkshayShah <shahaxay34@gmail.com>
2023-10-16 23:27:45 +0530

    form added in each meal

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -2,13 +2,28 @@ import React from "react";
 
 import './MealItem.css'
 
-const MealItem=props=>{
+const MealItem = props => {
     return (
         <React.Fragment>
-        <h3>{props.name}</h3>
-        <p>{props.description}</p>
-        <h3 className="mean-item__price">{` $ ${props.price}`}</h3>
-        <hr/>
+            <div className="meal-item">
+                <div>
+                    <h3>{props.name}</h3>
+                    <p>{props.description}</p>
+                    <h3 className="meal-item__price">{` $ ${props.price.toFixed(2)}`}</h3>
+                </div>
+                <div>
+                    <form className="meal-item__form">
+                        <span>
+                            <label htmlFor="amount"><b>Amount</b> &nbsp;&nbsp;</label>
+                            <input id="amount" type="number" inputMode="numeric" className="meal-item__input" /><br />
+                        </span>
+                        <span>
+                        <button type="submit" className="meal-item__button">+ Add</button>
+                        </span>
+                    </form>
+                </div>
+            </div>
+            <hr />
         </React.Fragment>
     );
 }
